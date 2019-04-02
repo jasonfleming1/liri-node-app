85375c04900c284a44ad43841ec56a541c10fd8f
phantom <phantomdesign9@gmail.com>
2019-04-02 13:10:09 -0500

    bands working commit

--- liri.js
+++ liri.js
@@ -7,25 +7,20 @@ var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 var omdb = require('omdb');
-//var omdb = new Omdb(keys.omdb);
-//var ombdURL = "http://www.omdbapi.com/?t=" + userQuery + (keys.omdb); //notsure this will work
+var moment = require('moment');
 
-//require bandsInTown
-//var bandsintown = keys.bandsintown;
-//from slack 3/30 :: queryURL = "https://rest.bandsintown.com/artists/" + searchValue + "/events?app_id=codingbootcamp";
-
-
-///////////////////////////////////DECLAR USER COMMAND INPUTS///////////////////////////////////
+///////////////////////////////////DECLARE USER COMMAND INPUTS///////////////////////////////////
 var searchType = process.argv[2];
 var searchValue = process.argv.slice(3).join(" ");
 
 ///////////////////////////////////USER COMMAND INPUT EVALS///////////////////////////////////
-  //case 'concert-this': concertThis(); break
-  //case 'movie-this': movieThis(); break
   //case 'do-what-it-says': doThis(); break
   //default: console.log('I\'m sorry, I don\'t understand')
 switch (searchType) {
   //case 'spotify-this-song': spotifyThis(); break
+    case 'concert-this':
+    concertThis();
+    break;
     case 'spotify-this-song':
     spotifyThis();
     break;
@@ -33,47 +28,62 @@ switch (searchType) {
     movieThis();
     break;
     default:
-    console.log(`***********************************************`)
-    console.log("\r\n\r\n\r\n");
+    console.log(`********************************`)
+    console.log("\n");
     console.log('I\'m sorry, I don\'t understand');
-    console.log("\r\n\r\n\r\n");
-    console.log(`***********************************************`)
+    console.log("\n");
+    console.log(`********************************`)
     break;
 }
 
 ///////////////////////////////////BANDS RETURN///////////////////////////////////
-//bands in town and moment super psuedo prep
 
-/*function concertThis() {
-    //request(queryURL with serachValue, function(error, response))
-    //if = (!error) {
-        format the api response & MUST INCLUDE:
-        1. venue name
-        2. venue location
-        3. Date of event formatted moment.MM/DD/YYYY
-    } else {
-        console.log('The event or band cannot be found!')
-    }
-}*/
+//notes from api site
+//make a call via artists/{artistname}/events
+//date string (query) === Can be one of the following values: "upcoming", "past", "all",  If not specified, only upcoming shows are returned
+//code 200 = Shows within a date range for the selected artist
+//must include venue name, venue location, date of event formatted using moment
+//trying a jsonparse 
+//datetime*	string($datetime)example: 2017-03-19T11:00:00
+
+function concertThis() {
+    request("https://rest.bandsintown.com/artists/" + searchValue + "/events?app_id=codingbootcamp", function(error, response, result)  {
+        if (!searchValue) {
+            console.log('You searched for nothing! Please try again!')
+        } else if (!error && response.statusCode === 200) {
+            var searchResult = JSON.parse(result);
+            if (searchResult.length > 0) {
+                for (i = 0; i < 1; i++) {
+                    console.log(`\nSuccess! I have your concert information...\n\nArtist: ${searchResult[i].lineup[0]} \nVenue: ${searchResult[i].venue.name} \nVenue Location: ${searchResult[i].venue.city}, ${searchResult[i].venue.region}, ${searchResult[i].venue.country}`)
+                    var eventDate = moment(searchResult[i].datetime).format('MM/DD/YYYY hh:00 A');
+                    console.log(`Your event\'s Date and Time is: ${eventDate}\n\n********************************`);
+                };
+            };
+
+        } else {
+            console.log('The event or band cannot be found!');
+        };
+    });
+};
 
 ///////////////////////////////////SPOTIFY RETURN///////////////////////////////////
 function spotifyThis() {
 
     if(!searchValue) {
-        searchValue = "The Sign Ace of Base"
+        searchValue = "The Sign"
     };
 
     spotify.search({
         type: 'track', 
         query: searchValue,
-        limit: 15
+        limit: 1 //limit of the array holding the spotify responses
     }, function (error, data) {
         if (error) {
             return console.log('Error occurred: ' + error);
         }
         var spotifyArr = data.tracks.items;
         for (i = 0; i < spotifyArr.length; i++) {
-            console.log(`\nSuccess!...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\n ****************`)
+            console.log(`\nSuccess!...I have your song...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\n********************************`)
         };
     });
 }
@@ -87,7 +97,7 @@ function movieThis(){
     };
     axios.get("https://www.omdbapi.com/?t=" + searchValue + "&y=&plot=short&apikey=trilogy")
     .then(function(response) {
-        console.log(`\n\n****************\nSuccess!...\n\nMovie Title: ${response.data.Title} \nYear of Release: ${response.data.Year} \nIMDB Rating: ${response.data.imdbRating} \nRotten Tomatoes Rating: ${response.data.Ratings[1].Value} ${response.data.Year} \nCountry Produced: ${response.data.Country} \nLanguage: ${response.data.Language} \nPlot: ${response.data.Plot} \nActors/Actresses: ${response.data.Actors} \n\n*****************`)
+        console.log(`\nSuccess!...I have your movie...\n\nMovie Title: ${response.data.Title} \nYear of Release: ${response.data.Year} \nIMDB Rating: ${response.data.imdbRating} \nRotten Tomatoes Rating: ${response.data.Ratings[1].Value} ${response.data.Year} \nCountry Produced: ${response.data.Country} \nLanguage: ${response.data.Language} \nPlot: ${response.data.Plot} \nActors/Actresses: ${response.data.Actors}\n\n********************************`)
     })
     .catch(function (error) {
         console.log(error);
